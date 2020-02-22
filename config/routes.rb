da3a4deb26b9fcdc62059c2347eec9efeb823f4a
seb <smm9308@gmail.com>
2020-02-22 17:15:27 -0500

    front

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :pages
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "bse#index"
 end
