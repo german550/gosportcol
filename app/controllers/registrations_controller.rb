b1fdde4555998190b74ee33ba6c92d9141c27b05
Patrick <patrickmarin@gmail.com>
2020-02-22 18:19:21 -0500

    users registered

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,30 @@
+class RegistrationsController < ApplicationController
+  def index
+    @registrations = Registrations.all
+  end
+  def new
+    @registration = Registration.new
+    @event = Event.find(params[:event_id])
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    @event = Event.find(params[:event_id])
+    @registration.user_id = current_user.id
+    @registration.event_id = params[:event_id]
+    if @registration.save!
+      redirect_to root_path
+     flash[:notice] = "Your Registration is Completed"
+    else
+     redirect_back fallback_location: root_path
+     flash[:alert] = "Registration Failed"
+    end
+  end
+
+
+  private
+  def registration_params
+    params.require(:registration).permit(:registered)
+  end
+
+end
