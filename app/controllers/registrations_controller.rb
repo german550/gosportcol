ecf44199de4a560fe92570acd729a6cd23db4737
Patrick <patrickmarin@gmail.com>
2020-04-02 18:39:20 -0500

    My Events Edit Delete Buttons

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,6 +20,26 @@ class RegistrationsController < ApplicationController
      flash[:alert] = "Registration Failed"
     end
   end
+  def edit
+    @registration = Registration.find(params[:id])
+    if current_user != @registration.user
+      sign_out current_user
+      redirect_to root_path
+      flash[:notice] = "Unauthorized Request"
+    end
+  end
+  def update
+    @registration = Registration.find(params[:id])
+    @registration.update(registration_params)
+    if current_user == @registration.user
+        @registration.update(registration_params)
+        redirect_to "/events/#{@registration.id}/edit"
+        flash[:notice] = "Event Succesfully Updated"
+      else
+        redirect_back(fallback_location: root_path)
+        flash[:alert] = "Not authorized to edit this registration"
+      end
+  end
 
 
   private
