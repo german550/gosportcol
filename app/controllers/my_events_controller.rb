635e7865933e5e6206302071346a83f5b753a8d6
Patrick <patrickmarin@gmail.com>
2020-04-03 13:38:45 -0500

    unregister button

--- app/controllers/my_events_controller.rb
+++ app/controllers/my_events_controller.rb
@@ -4,4 +4,26 @@ class MyEventsController < ApplicationController
         @myregistrations = current_user.registrations.all
         @myevents = current_user.events.all
     end
+    def edit
+        @myregistration = Registration.find(params[:id])
+        if current_user != @myregistration.user
+          sign_out current_user
+          redirect_to root_path
+          flash[:notice] = "Unauthorized Request"
+        end
+      end
+    def update
+        @myregistrations = Registration.find(params[:id])
+        if params [:registered]
+            @myregistrations.registered = true
+            @myregistrations.save
+            redirect_to rooth_path
+        end
+    end
+
+
+    def myregistrations_params
+        params.require(:registration).permit(:registered)
+      end
+
 end
